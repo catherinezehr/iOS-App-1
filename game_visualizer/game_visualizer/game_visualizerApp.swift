c9df7145083eece951c3f3bc24513831fcd306e6
mlins888 <Makenna@Linsky.net>
2026-05-20 11:36:18 -0400

    initial commit

---
+++ game_visualizer/game_visualizer/game_visualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  game_visualizerApp.swift
+//  game_visualizer
+//
+//  Created by Makenna Linsky on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct game_visualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
